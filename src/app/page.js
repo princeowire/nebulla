414d6f81fb87c73e69ff48bc636bc3a038cdba1e
Prince Owire <116007566+princeowire@users.noreply.github.com>
2025-08-08 22:19:41 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,7 @@ import Transactions from "@/components/UI/Transaction";
 import CountrySource from "@/components/UI/Country";
 
 export default function Home() {
-  const [isSidebarOpen, setSidebarOpen] = useState(true);
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
 
   const data = [
     { id: 1, name: "Sales", value: "6.5k", percentage: "4.3%", img: customer, color: "#ffba42", textColor: "text-[#ffba42]" },
